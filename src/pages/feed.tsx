9c42028f4dc19693c3d40884713d1375e5bbcca4
jihen501 <jihene.mkhinini@insat.ucar.tn>
2024-05-26 16:22:25 +0100

    trans

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -54,32 +54,10 @@ export default function Feed() {
                       <span className="flex h-2 w-2 translate-y-1.5 rounded-full bg-blue-500" />
                       <div className="grid gap-1">
                         <p className="text-sm font-medium">
-                          Your call has been confirmed.
+                          
                         </p>
                         <p className="text-sm text-gray-500 dark:text-gray-400">
                           5 min ago
-                        </p>
-                      </div>
-                    </div>
-                    <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-                      <span className="flex h-2 w-2 translate-y-1.5 rounded-full bg-blue-500" />
-                      <div className="grid gap-1">
-                        <p className="text-sm font-medium">
-                          You have a new message!
-                        </p>
-                        <p className="text-sm text-gray-500 dark:text-gray-400">
-                          1 min ago
-                        </p>
-                      </div>
-                    </div>
-                    <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-                      <span className="flex h-2 w-2 translate-y-1.5 rounded-full bg-blue-500" />
-                      <div className="grid gap-1">
-                        <p className="text-sm font-medium">
-                          Your subscription is expiring soon!
-                        </p>
-                        <p className="text-sm text-gray-500 dark:text-gray-400">
-                          2 hours ago
                         </p>
                       </div>
                     </div>
